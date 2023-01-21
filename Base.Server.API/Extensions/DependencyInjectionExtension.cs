9ec41e7a15660fa2351b13ff838e2795f92e07ad
matelko <mateusz280798@gmail.com>
2023-01-21 22:23:15 +0100

    Add DbContext

---
+++ Base.Server.API/Extensions/DependencyInjectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using Base.Server.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace Base.Server.API.Extensions;
+
+public static class DependencyInjectionExtension
+{
+    public static void RegisterDbContext(this WebApplicationBuilder builder)
+    {
+        string connectionString = builder.Configuration.GetConnectionString("Default")
+            ?? throw new InvalidOperationException("Connection string 'Default' not found.");
+
+        builder.Services.AddDbContext<AppDbContext>(
+            options => options
+                .UseSqlServer(connectionString)
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking),
+            ServiceLifetime.Singleton);
+
+        builder.Services.AddDbContextFactory<AppDbContext>(
+            options => options
+                .UseSqlServer(connectionString)
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking),
+            ServiceLifetime.Scoped);
+    }
+}
--- Base.Server.API/Program.cs
+++ Base.Server.API/Program.cs
@@ -1,16 +1,20 @@
+using Base.Server.API.Extensions;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 {
-
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
+    // DbContext
+    builder.RegisterDbContext();
+
+    // Services
 }
 
 
 WebApplication app = builder.Build();
 {
-    // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
